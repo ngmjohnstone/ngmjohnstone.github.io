d4c7eb5935bd1a96211ace1e5ae3b9766bcec9e3
ngmjohnstone <68429510+ngmjohnstone@users.noreply.github.com>
2024-10-09 18:00:41 +0100

    charity: Added charity section
    - Added charity section with charities in grid format
    - Updated nav-link to have hover colour effect

--- main.js
+++ main.js
@@ -4,10 +4,12 @@ import './style.css'
 import { createNavBar } from './navbar.js';
 import { createHeroSection } from './hero.js';
 import { createAboutSection } from './about.js';
+import { createCharitySection } from './charity.js';
 // import { createBlogSection } from './blog.js';
 
 document.querySelector('#app').innerHTML =
   createNavBar() +
   createHeroSection() +
-  createAboutSection() // + createBlogSection()
+  createAboutSection() + // + createBlogSection()
+  createCharitySection()
 ;
