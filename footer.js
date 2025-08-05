e31b48ef3b08b148ad4ba625338c378793bf082e
ngmjohnstone <68429510+ngmjohnstone@users.noreply.github.com>
2025-08-05 18:13:37 +0100

    add autoupdating footer

--- footer.js
+++ footer.js
@@ -1,6 +1,23 @@
 // footer.js
 
-const footerText = `Last updated Jul 2025`;
+const path = `ngmjohnstone/ngmjohnstone.github.io`;
+async function fetchLatestCommit() {
+  try {
+      const response = await fetch(`https://api.github.com/repos/` + path + `/commits`);
+      const commits = await response.json();
+      if (commits.length > 0) {
+        const options = { day: '2-digit', month: 'short', year: 'numeric' }
+        
+          return `Last updated ${new Date(commits[0].commit.author.date).toLocaleDateString('en-GB', options)}`;
+      }
+  } catch (error) {
+      console.error('Error fetching commit history:', error);
+  }
+  return `Last updated: Unable to fetch data`;
+}
+
+const footerText = await fetchLatestCommit();
+const repoHistoryUrl = `https://github.com/` + path + `/commits/main/`;
 
 function createFooterSection() {
     const footerSectionHTML = `
@@ -8,7 +25,9 @@ function createFooterSection() {
         <br>
         <hr>
         <p>
+          <a href=${repoHistoryUrl} target="_blank">
             ${footerText}
+          </a>
         </p>
       </footer>
     `;
